1022337d74597521d54dbbc1be841af8e56b464a
yeoularu <yeoularu@gmail.com>
2022-03-31 22:03:42 +0900

    import Header, Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import { useState } from "react";
+import Card from "./Card.jsx";
+import Header from "./Header.jsx";
 
 function App() {
   return (
-    <div className="w-full h-screen flex flex-col justify-between align-top bg-red-200">
-      <h1 className="basis-1/4 place-content-center font-bold text-2xl text-blue-900">
-        React and Tailwind with Vitejs!
-      </h1>
-      <img className="basis-1/2 hover:animate-spin" src={logo} />
+    <div className="bg-neutral-900">
+      <Header />
+      <div className="w-full h-screen flex flex-col justify-center items-center">
+        <Card />
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
