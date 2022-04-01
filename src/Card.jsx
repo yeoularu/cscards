b7314c9ac0503f7c91034563eafcbe29c4882750
yeoularu <yeoularu@gmail.com>
2022-04-01 22:47:36 +0900

    parse answer from data

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,23 +1,30 @@
 import { useState } from "react";
+import data from "./data.js";
 
 function Card() {
   let [hidden, setHidden] = useState(true);
 
-  function setAnswerCSS() {
-    return `${hidden ? "text-neutral-800" : "text-sky-400"}`;
-  }
+  const text = data[0].text;
+  const start = text.indexOf("**");
+  const end = text.indexOf("**", start + 2);
+  const answer = text.substring(start + 2, end);
+
+  const cardCSS = `flex justify-center items-center w-80 h-96 rounded-2xl bg-neutral-800 text-lg ${
+    hidden ? null : "border border-white"
+  }`;
+  const answerCSS = `${hidden ? "text-neutral-800" : "text-sky-400"}`;
 
   return (
-    <div
-      className="flex justify-center items-center w-80 h-96 rounded-2xl bg-neutral-800 text-lg"
-      onClick={() => setHidden(!hidden)}
-    >
+    <div className={cardCSS} onClick={() => setHidden(!hidden)}>
       <p className="w-64 rounded-full text-white text-center break-keep">
-        스택은 제일 마지막으로 삽입된 원소가 제일 먼저 삭제되는{" "}
-        <span className={setAnswerCSS()}>후입선출</span> 자료구조이다.
+        {text.substring(0, start)}
+        <span className={answerCSS}>{answer}</span>
+        {text.substring(end + 2)}
       </p>
     </div>
   );
 }
+
+function Text() {}
 
 export default Card;
